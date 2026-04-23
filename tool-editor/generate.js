b7ee94bda165b14ec7a29847fec96ad70e644ebd
Namsy <cchundred@gmail.com>
2026-04-23 20:32:00 +0100

    c

--- tool-editor/generate.js
+++ tool-editor/generate.js
@@ -1,17 +1,23 @@
 function createUnencodedData() {
     let dataArray = [];
-    let pointCoordinateInput = document.querySelectorAll(".coordinate-input");
+    let pointCoordinateInputs = document.querySelectorAll(".coordinate-input");
+    let pointValue = 0;
+
     
-    for (let pointCoordinate of pointCoordinateInput) {
-        let pointValue = pointCoordinate.getAttribute("value");
+    for (let pointCoordinate of pointCoordinateInputs) {
+        pointValue = pointCoordinate.value;
         dataArray.push(pointValue);
     }
     console.log(dataArray);
+    console.log(pointCoordinateInputs);
 }
 
 function main() {
     const generateButton = document.querySelector("#generate-button");
     generateButton.addEventListener("click", createUnencodedData);
+    
+    const addPointButton = document.querySelector("#add-point-button")
+    addPointButton.addEventListener("click",)
 }
 
-main()
+main();
