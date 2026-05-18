1920694f9667a00fad31237eecb113a8ac853e9b
thenamsy <144534684+thenamsy@users.noreply.github.com>
2026-05-18 21:20:08 +0100

    major css change + error handling

--- tool-editor/generate.js
+++ tool-editor/generate.js
@@ -6,14 +6,14 @@ function createUnencodedArray() {
         for (let pointCoordinate of pointCoordinateInputs) {
             pointValue = pointCoordinate.valueAsNumber;
             
-            /* if (pointValue === NaN) {
+            if (isNaN(pointValue)) {
                 return "NAN-ERROR";
             }
 
-            if (pointValue < -138 || pointValue > 117 || pointValue) {
+            if (pointValue < -138 || pointValue > 117) {
                 return "POINT-VALUE-RANGE-ERROR";
 
-            }    */
+            }
 
 
             dataArray.push(pointValue);
@@ -41,23 +41,35 @@ function createUnencodedArray() {
                     case "torque-button":
                         dataArray.push(2);
                         break;
-                    /* default:
-                        return "NO-TOOL-TYPE-ERROR"; */
                 }
-            }
+            }    
+        }
+
+        if (dataArray.length % 2 === 0) {
+            return "NO-TOOL-TYPE-ERROR";
         }
     console.log(dataArray);
     return dataArray;
 }
 
-    function generateCode(){
+function generateCode(){
         let unencodedArray = createUnencodedArray();
-        outputText = document.querySelector("#output-text");
+        console.log(unencodedArray);
 
-       /* if (unencodedArray === "NAN-ERROR") {
-            outputText.value = "Please input a number into all coordinate boxes";
+       if (unencodedArray === "NAN-ERROR") {
+            displayError("Please input a number into all coordinate boxes.");
             return;
-        } */
+        }
+
+        if (unencodedArray === "POINT-VALUE-RANGE-ERROR") {
+            displayError("All numbers must be between -117 and 138.");
+            return;
+        }
+
+        if (unencodedArray === "NO-TOOL-TYPE-ERROR") {
+            displayError("Please select a tool type.");
+            return;
+        }
 
         for (let i = 0; i < (unencodedArray.length - 1); i++) {
             unencodedArray[i] += 10; // make coordinates relative to top left corner of edit bay instead of ship center
@@ -72,22 +84,37 @@ function createUnencodedArray() {
         //console.log(encodedUInt8);
         
 
-        outputText.value = encodedUInt8;
+        displayCode(encodedUInt8);
     }
+
+function displayError(text) {
+    outputText.style.color = "red";
+    outputText.value = text;
+}
+
+function displayCode(text) {
+    outputText.style.color = "black";
+    outputText.value = text;
+}
 
 function addPoint() {
     const coordinateInputs = document.querySelector("#coordinate-inputs");
 
     let newElement = document.createElement("div");
-    newElement.id = `point-${newPointNumber}-coordinates`
-    newElement.innerHTML = `
-        <label>Point ${newPointNumber}: </label>
+    newElement.id = `point-${newPointNumber}-coordinates`;
+    newElement.className = "point";
+    newElement.innerHTML =
+    `
+        <label>Point ${newPointNumber}</label>
+
+        <span class="point-dash">-</span>
         <span>
-            <label for="point-${newPointNumber}-x">x:</label>
+            <label for="point-${newPointNumber}-x" class="x-text">x:</label>
             <input type="number" id="point-${newPointNumber}-x" max="117" min="-138" class="coordinate-input">
-            <label for="point-${newPointNumber}-y">y:</label>
+            <label for="point-${newPointNumber}-y" class="y-text">y:</label>
             <input type="number" id="point-${newPointNumber}-y" max="117" min="-138" class="coordinate-input">
-        </span>`;
+        </span>
+    `;
     
     // let removeNewPointButton = newElement.querySelector("button");
     // removeNewPointButton.addEventListener("click", removePoint);
@@ -96,26 +123,13 @@ function addPoint() {
     newPointNumber += 1;
 }
 
-/* function removePoint(event) {
-    let pointIndex = Number(event.currentTarget.dataset.pointIndex) - 1;
-    const coordinateInputs = document.querySelector("#coordinate-inputs");
-    let coordinateInputArray = Array.from(coordinateInputs.children);
-
-    coordinateInputArray.splice(pointIndex, 1);
-
-    console.log(pointIndex);
-    console.log(coordinateInputArray);
-}
-*/
-
 function removeLastPoint() {
     let points = document.querySelector("#coordinate-inputs");
     points.lastChild.remove();
-    newPointNumber -= 1;
+    newPointNumber -= (newPointNumber > 1) ? 1 : 0;
 }
 
 function copyCode(){
-    outputText = document.querySelector("#output-text");
     navigator.clipboard.writeText(outputText.value);
 }
 
@@ -142,4 +156,5 @@ function main() {
 
 let newPointNumber = 1;
 let pointDisplayNumber = 1;
+let outputText = document.querySelector("#output-text");
 main();
